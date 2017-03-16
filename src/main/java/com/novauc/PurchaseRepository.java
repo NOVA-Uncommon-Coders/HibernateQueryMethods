f8879824ab5ea55e933e910cf12dcafcf72bb4f7
William Criss <wcriss44@gmail.com>
2017-03-16 15:38:01 -0400

    Finish all requirements

---
+++ src/main/java/com/novauc/PurchaseRepository.java
@@ -0,0 +1,9 @@
+package com.novauc;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface PurchaseRepository extends CrudRepository<Purchase, Integer> {
+    List<Purchase> findAllByCategory(String category);
+}
---
+++ src/main/java/com/novauc/WalmartController.java
@@ -0,0 +1,82 @@
+package com.novauc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by willc on 3/16/17.
+ */
+@Controller
+public class WalmartController {
+    @Autowired
+    CustomerRepository customers;
+    @Autowired
+    PurchaseRepository purchases;
+
+    /***********************
+     * GET routes
+     ***********************/
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String index(Model model, HttpSession session, String category) {
+        List<Purchase> purchaseList;
+
+        if (category != null){
+            purchaseList = (List<Purchase>) purchases.findAllByCategory(category);
+        } else {
+            purchaseList= (List<Purchase>) purchases.findAll();
+        }
+
+        model.addAttribute("purchases", purchaseList);
+        return "home";
+    }
+
+    /***********************
+     * POST routes
+     ***********************/
+
+
+    @PostConstruct
+    public void init() {
+        try {
+        File file;
+        Scanner scanner;
+        if (customers.count() == 0) {
+            file = new File("customers.csv");
+            scanner = new Scanner(file);
+            scanner.useDelimiter("\n");
+            while(scanner.hasNext()){
+                String[] data = scanner.next().split(",");
+                customers.save(new Customer(data[0], data[1]));
+            }
+        }        if (purchases.count() == 0) {
+                file = new File("purchases.csv");
+                scanner = new Scanner(file);
+                scanner.useDelimiter("\n");
+                while(scanner.hasNext()){
+                    String[] data = scanner.next().split(",");
+                    purchases.save(new Purchase(checkCustomer(data[0]), data[1], data[2], data[3], data[4]));
+                }
+            }
+    } catch (Exception e){
+            System.out.println("Huston had a problem with scanning @" +  e.getMessage());
+        }
+    }
+    public Customer checkCustomer(String id){
+        Customer customer = customers.findFirstById(Integer.valueOf(id));
+        if (customer == null){
+            System.out.println("customer was null");
+            //TODO TEST STATEMENT
+        }
+        return customer;
+    }
+}
